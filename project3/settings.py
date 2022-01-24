05779ae83d1ebbbdb05f4a050281c149d8c071bc
katariadhairyaDM <97227741+katariadhairyaDM@users.noreply.github.com>
2022-01-24 15:33:19 +0530

    Update settings.py

--- project3/settings.py
+++ project3/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-yr9ehpf$g%xskf(_g!e(ad1_%4swbc8rz+adxkk+wh5t=8v0g1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'voice-engine.herokuapp.com']
 
 
 # Application definition
